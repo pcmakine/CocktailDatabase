f907d6afc2794a7f3a50c6a05844c09f0f404e7a
Mäkinen Petri M <pcmakine@da1-cs-b221-11.helsinki.fi>
2014-01-14 14:00:56 +0200

    first commit

---
+++ Esittelysivu.php
@@ -0,0 +1,12 @@
+<?php 
+  $mainheading = "Tietokantasovelluksen esittelysivu"; 
+  $firstsubheading = "Yleisiä linkkejä"; 
+?><!DOCTYPE HTML>
+<html>
+<head><title><?php echo $tervehdys; ?></title></head>
+<body>
+  <h1><?php echo $mainheading; ?></h1>
+  <h2><?php echo $firstsubheading; ?></h2>
+  <h2><?php echo "Työn aihe"; ?></h2>
+</body>
+</html>
