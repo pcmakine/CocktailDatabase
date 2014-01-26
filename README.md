7ebb45047552bcbdfce1b325bbf9382865efc238
Petri Makinen <petri.mkinen1@gmail.com>
2014-01-26 21:32:32 +0200

    toisen viikon palautus

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ CocktailDatabase
 ================
 
 A database of cocktails. UI will be implemented as a web application using php.
+
+http://pcmakine.users.cs.helsinki.fi/cocktaildatabase/Esittelysivu.html
--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -4,6 +4,9 @@ insert into cocktail(id, cocktailname, recipe, price) values(default, 'water', n
                                                                 (default, 'orange juice', null, 0.3);
 insert into ingredient(ingname) values('water'),
                                         ('orange');
-insert into cocktail_ingredient_link(ingname, cocktailid) values('orange', 2);
-
-
+insert into cocktail_ingredient_link(ingname, cocktailid) values('orange', 2),
+                                                                ('water', 1);
+insert into rating(username, cocktailid, rating) values('pete', 1, 5),
+                                                        ('mickey mouse',1, 3),
+                                                        ('pete', 2, 4),
+                                                        ('mickey mouse', 2, 4);
